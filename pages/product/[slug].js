4aa69e28aa2ae41b580151291cf035b9adcdae2e
bitcooker <dreamhighpassionhigh@gmail.com>
2022-12-01 00:55:33 +0200

    add maylike products

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { client, urlFor } from '../../lib/client'
+import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
+import { Product } from '../../components';
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
@@ -10,6 +12,57 @@ const ProductDetails = ({ product, products }) => {
         <div>
           <div className="image-container">
             <img src={urlFor(image && image[0])} alt="" />
+          </div>
+          {/* 
+          <div className="small-images-container">
+            {image?.map((item, i) => (
+              <img
+                src={urlFor(item)}
+                className=''
+                onMouseEnter=''
+              />
+            ))}
+          </div> */}
+        </div>
+
+        <div className="product-detail-desc">
+          <h1>{name}</h1>
+
+          <div className="reviews">
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
+            </div>
+            <p>(20)</p>
+          </div>
+          <h4>Details:</h4>
+          <p>{details}</p>
+          <p className='price'>${price}</p>
+          <div className="quantity">
+            <h3>Quantity</h3>
+            <p className='quantity-desc'>
+              <span className='minus' onClick=''><AiOutlineMinus /></span>
+              <span className='num' onClick=''>0</span>
+              <span className='plus' onClick=''><AiOutlinePlus /></span>
+            </p>
+          </div>
+          <div className="buttons">
+            <button type="button" className="add-to-cart" onClick=''>Add to Cart</button>
+            <button type="button" className="buy-now" onClick=''>Buy Now</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="maylike-products-wrapper">
+        <h2>You may also like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+            {products.map((item) => (
+              <Product key={item._id} product={item} />
+            ))}
           </div>
         </div>
       </div>
