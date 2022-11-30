2612576af22912a0ce508514e94fe307a789da9f
bitcooker <dreamhighpassionhigh@gmail.com>
2022-11-30 22:33:11 +0200

    update FooterBanner

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ const Home = ({ products, bannerData }) => {
         {products?.map((product) => <Product key={product._id} product={product} />)}
       </div>
 
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </>
   )
 }
