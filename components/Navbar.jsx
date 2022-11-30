45536bca2df2eef9c04eda7ed5b69225f722ace6
bitcooker <dreamhighpassionhigh@gmail.com>
2022-11-30 23:08:38 +0200

    update Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import { AiOutlineShopping } from "react-icons/ai";
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">Nicon</Link>
+      </p>
 
-export default Navbar
+      <button type="button" className="cart-icon" onClick="">
+        <AiOutlineShopping />
+        <span className="cart-item-qty">1</span>
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
