65c452a132915fdad41dad9a8fee3c855d8e60aa
bitcooker <dreamhighpassionhigh@gmail.com>
2022-11-30 23:23:21 +0200

    update Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import { AiOutlineTwitter, AiFillInstagram } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className="footer-container">
+      <p>2022 Digital Cameras All rights reserved</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
