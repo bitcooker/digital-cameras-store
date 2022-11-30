d93618cc31c619c57d6552c3725e403a579c6fba
bitcooker <dreamhighpassionhigh@gmail.com>
2022-11-30 23:03:13 +0200

    add Layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
-    <div>
-      
+    <div className="layout">
+      <Head>
+        <title>Digital Cameras</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
