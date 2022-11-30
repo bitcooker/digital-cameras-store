11f9777be554eae7cf8ebad20f4ae73ed15746fd
bitcooker <dreamhighpassionhigh@gmail.com>
2022-11-30 22:17:03 +0200

    update Product

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import { urlFor } from "../lib/client";
 
-const Product = () => {
+const Product = ({ product: { image, slug, price, name } }) => {
   return (
     <div>
-      
+      <Link href={`/product/${slug.current}`}>
+        <div className="product-card">
+          <img
+            src={urlFor(image && image[0])}
+            alt=""
+            width={250}
+            height={250}
+            className="product-image"
+          />
+          <p className="product-name">{name}</p>
+          <p className="product-price">${price}</p>
+        </div>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ const Home = ({ products, bannerData }) => {
       </div>
 
       <div className='products-container'>
-        {products?.map((product) => product.name)}
+        {products?.map((product) => <Product key={product._id} product={product} />)}
       </div>
 
       <FooterBanner />
