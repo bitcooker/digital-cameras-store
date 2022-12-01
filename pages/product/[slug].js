fe79dc52d12d0a018d40a7caf048ab74c0a936b8
bitcooker <dreamhighpassionhigh@gmail.com>
2022-12-01 12:18:59 +0200

    update qty

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -2,10 +2,12 @@ import React, { useState } from 'react'
 import { client, urlFor } from '../../lib/client'
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import { Product } from '../../components';
+import { useStateContext } from '../../context/StateContext';
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0)
+  const { decQty, incQty, qty } = useStateContext();
 
   return (
     <div>
@@ -46,9 +48,9 @@ const ProductDetails = ({ product, products }) => {
           <div className="quantity">
             <h3>Quantity</h3>
             <p className='quantity-desc'>
-              <span className='minus' onClick=''><AiOutlineMinus /></span>
-              <span className='num' onClick=''>0</span>
-              <span className='plus' onClick=''><AiOutlinePlus /></span>
+              <span className='minus' onClick={decQty}><AiOutlineMinus /></span>
+              <span className='num' onClick=''>{qty}</span>
+              <span className='plus' onClick={incQty}><AiOutlinePlus /></span>
             </p>
           </div>
           <div className="buttons">
