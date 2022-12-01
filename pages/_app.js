08709d475f37ac97fcfa6d0ab412420464728b15
bitcooker <dreamhighpassionhigh@gmail.com>
2022-12-01 12:10:17 +0200

    add context to the app

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Toaster } from 'react-hot-toast';
 import { Layout } from '../components';
+import { StateContext } from '../context/StateContext';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
   )
 }
 
