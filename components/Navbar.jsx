4902d0e6daf79a6a5f2f2c8a424d69ac07f6a152
bitcooker <dreamhighpassionhigh@gmail.com>
2022-12-01 12:58:18 +0200

    update onClick for cart btn

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,18 +1,28 @@
 import Link from "next/link";
 import React from "react";
 import { AiOutlineShopping } from "react-icons/ai";
+import { Cart } from "./Cart";
+import { useStateContext } from "../context/StateContext";
 
 const Navbar = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
+
   return (
     <div className="navbar-container">
       <p className="logo">
         <Link href="/">Nicon</Link>
       </p>
 
-      <button type="button" className="cart-icon" onClick="">
+      <button
+        type="button"
+        className="cart-icon"
+        onClick={() => setShowCart(true)}
+      >
         <AiOutlineShopping />
-        <span className="cart-item-qty">1</span>
+        <span className="cart-item-qty">{totalQuantities}</span>
       </button>
+
+      {showCart && <Cart />}
     </div>
   );
 };
